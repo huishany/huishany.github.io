4ef4bea5f75908687bf612cdd74111df78056930
huishany <huishany1012@gmail.com>
2025-12-29 23:33:46 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import pugPlugin from 'vite-plugin-pug'
 
 export default defineConfig({
   plugins: [pugPlugin()],
+  base: process.env.VITE_BASE_PATH || "/huishany.github.io",
   build: {
     outDir: '../dist',
     emptyOutDir: true
